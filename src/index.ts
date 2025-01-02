ad4e48df5832d0999433eef47819bc347d7baa51
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-01-02 15:54:24 -0300

    parte 19

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,95 @@
-import { Categoria } from "@modules/catalogo/domain/categoria/categoria.entity";
-import { PrismaClient } from "@prisma/client";
-import { DomainException } from "@shared/domain/domain.exeption";
-import { error } from "console";
-import { aw } from "vitest/dist/chunks/reporters.D7Jzd9GS";
+import { Categoria } from '@modules/catalogo/domain/categoria/categoria.entity';
+import { CategoriaPrismaRepository } from '@modules/catalogo/infra/database/categoria.prisma.repository';
+import { PrismaClient } from '@prisma/client';
+import { DomainException } from '@shared/domain/domain.exeption';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: ['query', 'info'],
+    errorFormat: 'pretty'
+});
 
-async  function main(){
+async function main() {
+    
+    prisma.$connect().then(
+        async () => {
+            console.log('Postgres Conectado');
+        }
+    );
 
-  //criar categoria
-  let categoria:Categoria;
+    const categoriaRepo = new CategoriaPrismaRepository(prisma);
 
-  categoria = Categoria.criar({nome: 'mesa'})
+    ////////////////////////////////
+    //Recuperar Categoria por UUID//
+    ////////////////////////////////
+    
+    // const categoriaRecuperada: Categoria | null = await categoriaRepo.recuperarPorUuid("91bb57f2-416c-4ee6-9c1e-86cb9b5c3d04");
 
-  //salvar no banco
-  await prisma.categoria.create({
-    data:{
-      id: categoria.id,
-      nome: categoria.nome
-    }
-  })
+    // console.log(categoriaRecuperada);
 
+    /////////////////////////////////
+    //Recuperar Todas as Categorias//
+    /////////////////////////////////
+    
+    // const todasCategorias: Array<Categoria> = await categoriaRepo.recuperarTodos();
 
-  //atualizar categoria 
-  const categoriaRecuperada = await prisma.categoria.update({
-    where: {id: 'e4fcdb9f-4345-41f7-b630-13441bc92861'},
-    data: {nome: 'banho'}
-  })
+    // console.log(todasCategorias);
 
-  
+    ////////////////////////////////
+    //Verifica se Existe Categoria//
+    ////////////////////////////////
+    
+    // const existeCategoria: boolean = await categoriaRepo.existe("91bb57f2-416c-4ee6-9c1e-86cb9b5c3d04");
 
-  //listar categorias
-  const ListaCategorias = await prisma.categoria.findMany();
-  console.log(ListaCategorias)
+    // console.log(existeCategoria);
+
+    /////////////////////
+    //Inserir Categoria//
+    /////////////////////
+    
+    // const categoria: Categoria = Categoria.criar({
+    //    nome:'Sala e Quarto'
+    // });     
+
+    // const categoriaInserida = await categoriaRepo.inserir(categoria);
+
+    // console.log(categoriaInserida);
+
+    ///////////////////////
+    //Atualizar Categoria//
+    ///////////////////////
+    
+    // const categoria: Categoria = Categoria.recuperar({
+    //    id: "e4fcdb9f-4345-41f7-b630-13441bc92861",
+    //    nome: "Banho"
+    // });     
+
+    // const atualizouCategoria: boolean = await categoriaRepo.atualizar(categoria.id,categoria);
+
+    // console.log(atualizouCategoria)
+
+    /////////////////////
+    //Deletar Categoria//
+    /////////////////////
+    
+    const categoriaDeletada: boolean = await categoriaRepo.deletar("e4fcdb9f-4345-41f7-b630-13441bc92861");
+    
+    console.log(categoriaDeletada);
 
 }
 
-
-
 main()
-  .then(async()=>{
-    await prisma.$disconnect()
-
-  })
-  .catch(async (error)=>{
-    if(error instanceof DomainException){
-      console.log('Execeção de Domínio');
-      console.log(error.message);
-    } 
-    else{
-      console.log('Outras Execeções');
-      console.log(error.message);
-    } 
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (error) => {
+       if (error instanceof DomainException) {
+           console.log('Execeção de Dóminio');
+           console.log(error.message);
+       }
+       else {
+           console.log('Outras Exceções');
+           console.log(error.message);
+       }
+       await prisma.$disconnect()
+       process.exit(1)
+   })
